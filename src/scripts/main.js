4181d34d4b0458cd56b8d0becb0cc5afe03db6db
Chaiyawat Nunes <chaiyawat.nunes@gmail.com>
2014-11-23 18:56:02 -0800

    Facepalm hard

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,8 @@
 /* This is the game's code. Edit, give credit! */
 var appFrame = document.getElementById("app");
-bonsai.run(appFrame, width, height, {
-  code: function() {
     var player = new Bitmap('player_version1.png').on('load', function() {
             this.addTo('stage'); 
           });
     //var player = new Rect(10, 10, 100, 100);
       //player.addTo(stage);
       //player.attr('fillColor', 'green');
-  },
-        width: appFrame.width,
-        height: appFrame.height
-    });
